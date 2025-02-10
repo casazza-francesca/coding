ba07503120d6b57a269d1e2f4e7051ef78a8b66e
casazza-francesca <francesca.casazza@liceoavogadrobiella.net>
2025-02-10 15:53:08 +0000

    Lezione 2 fine

--- lezione2/indovina_numero.py
+++ lezione2/indovina_numero.py
@@ -1,3 +1,33 @@
 ### 3. Indovina il Numero
 #Scrivere un gioco in cui il programma genera un numero casuale tra 1 e 100. L'utente deve indovinare il numero, con suggerimenti forniti ad ogni tentativo ("Troppo alto" o "Troppo basso"). Alla fine, il programma assegna un **indice di fortuna** basato sul numero di tentativi.
 
+# 3. Indovina il Numero
+    #Scrivere un gioco in cui il programma genera un numero casuale tra 1 e 100. L'utente deve indovinare il numero, con suggerimenti forniti ad ogni tentativo ("Troppo alto" o "Troppo basso"). Alla fine, il programma assegna un **indice di fortuna** basato sul numero di tentativi.
+#importare una standard library
+    #random.randint(a,b) numero casuale da 'a' a 'b'
+
+import random
+
+# 1. creare il numero casuale all'inizio all'oscuro dell'utente
+n = random.randint(1, 100)
+
+# 2. serve un ciclo while pk deve andare avanti fino a quando l'utente non ha azzeccato 
+#     permettere all'utente di inserire un numero
+#     condizionale if else per stabilire se il numero ch è stato inserito è < > o = a quello che è ststpo inserito
+
+win = False
+t=0
+ while win == False:
+    guess = int(input('Ciao, ho generato un numero casuale da 1 a 100. Prova a indovinare di che numero si tratta: '))
+    t = t + 1
+        if guess == n:
+        win = True
+        print (f'Hai indovinato! Complimenti hai vinto von {t} tentativi')
+    elif guess < n:
+        print (f'Mi dispiace ma hai sbagliato. Il numero che hai scelto è troppo piccolo. Prova di nuovo:')
+    else guess > n:
+         print (f'Mi dispiace ma hai sbagliato. Il numero che hai scelto è troppo grande. Prova di nuovo:')
+    
+# 3.quando l'utente indovina la condizione sarà falsa
+
+# 4. conteggio dei tentativi
